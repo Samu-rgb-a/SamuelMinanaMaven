938ea4dbf9e92ff530e93337683e408c38077b53
Samu <samuelminana@gmail.com>
2026-02-05 16:55:24 +0100

    Banner + guión completado

--- src/main/java/com/samuelminana/tema4maven/Main.java
+++ src/main/java/com/samuelminana/tema4maven/Main.java
@@ -1,11 +1,36 @@
 package com.samuelminana.tema4maven;
 
 import com.github.lalyos.jfiglet.FigletFont;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        List <String> guion = new ArrayList<>();
+
         String texto = "Samuel Minana";
         String banner = FigletFont.convertOneLine(texto);
-        System.out.println(banner);
+
+        for (String linea : banner.split("\n")) {
+            guion.add(linea);
+        }
+
+        guion.add("");
+
+        guion.add("Nombre: Samuel Minana");
+        guion.add("Perfil: Estudiante de DAM");
+        guion.add("Lenguajes: HTML, Java, JS");
+        guion.add("Herramientas: Git, GitHub, Maven");
+        guion.add("Experiencias: Trabajos en equipo");
+        guion.add("Intereses: Programación y sistemas");
+        guion.add("Habilidades: Trabajo en equipo");
+        guion.add("Contacto: samuelminana@gmail.com");
+        guion.add("Idiomas: Castellano, Inglés, Valenciano");
+        guion.add("Prácticas: Empresa online");
+
+        for (String linea : guion) {
+            System.out.println(linea);
+        }
     }
 }
